7d4cfa1a106ff3644d3346b189774850b7fe9163
Dennis Leon <dleon@pivotal.io>
2016-11-30 15:57:19 -0800

    Update pipeline & scripts for os-image & stemcell building
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

---
+++ ci/pipelines/stemcells/configure-pipeline.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eu
+
+fly -t production set-pipeline \
+ -p bosh:stemcells:3312.umask.x \
+ -v stemcell-branch=3312.umask.x \
+ -v stemcell_version_key=bosh-stemcell/version-3312.umask.x \
+ -v stemcell_version_semver_bump=minor \
+ -c ci/pipelines/stemcells/pipeline.yml \
+ -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
+ -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
