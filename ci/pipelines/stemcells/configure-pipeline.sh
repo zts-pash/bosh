08e5774295a08789baa0db9aaa09ae2024c12b9c
Danny Berger <dberger@pivotal.io>
2016-12-02 16:23:25 -0800

    Update pipeline to set umask in the semver
    [#127359667](https://www.pivotaltracker.com/story/show/127359667)
    
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- ci/pipelines/stemcells/configure-pipeline.sh
+++ ci/pipelines/stemcells/configure-pipeline.sh
@@ -6,7 +6,6 @@ fly -t production set-pipeline \
  -p bosh:stemcells:3312.umask.x \
  -v stemcell-branch=3312.umask.x \
  -v stemcell_version_key=bosh-stemcell/version-3312.umask.x \
- -v stemcell_version_semver_bump=minor \
  -c ci/pipelines/stemcells/pipeline.yml \
  -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
  -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
