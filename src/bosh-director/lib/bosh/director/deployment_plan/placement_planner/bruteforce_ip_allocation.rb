fad2f0ebcbdfdfc18ac5b22a9307abd052dd0b69
Josh Russett <jrussett@pivotal.io>
2018-08-02 14:18:28 -0700

    wip

--- src/bosh-director/lib/bosh/director/deployment_plan/placement_planner/bruteforce_ip_allocation.rb
+++ src/bosh-director/lib/bosh/director/deployment_plan/placement_planner/bruteforce_ip_allocation.rb
@@ -11,12 +11,21 @@ module Bosh
 
           def find_best_combination
             allocated_ips = AllocatedIps.new
-            try_combination(@networks_to_static_ips, allocated_ips)
+            @candidates = []
+            @candidates << { networks_to_static_ips: @networks_to_static_ips, allocated_ips: allocated_ips }
+            until @candidates.empty? do
+              candidate = @candidates.delete_at(0)
+
+              result = try_combination(candidate[:networks_to_static_ips], candidate[:allocated_ips])
+              return result unless result.nil?
+            end
+            return nil
           end
 
           private
 
           def try_combination(networks_to_static_ips, allocated_ips)
+            insert_pos = 0
             if all_ips_belong_to_single_az(networks_to_static_ips)
               if even_distribution_of_ips?(networks_to_static_ips)
                 return networks_to_static_ips
@@ -47,9 +56,8 @@ module Bosh
                   static_ip_to_azs.az_names = [az_name]
                   allocated_ips.allocate(az_name)
                   candidate_networks_to_static_ips = Bosh::Common::DeepCopy.copy(networks_to_static_ips)
-                  result = try_combination(candidate_networks_to_static_ips, AllocatedIps.new)
-                  next if result.nil?
-                  return result
+                  @candidates.insert(insert_pos, networks_to_static_ips: candidate_networks_to_static_ips, allocated_ips: AllocatedIps.new)
+                  insert_pos += 1
                 end
               end
             end
@@ -67,7 +75,6 @@ module Bosh
                   hash[network][az_name] += 1
                 end
               end
-
             end
 
             hash.values.uniq.size > 1 ? false : true
