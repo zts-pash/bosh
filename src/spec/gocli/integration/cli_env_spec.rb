17e3e284f969ab5a717d521f91facbcac588f069
Sebastian Heid <sebastian.heid@sap.com>
2018-11-02 12:18:17 -0700

    wip
    Signed-off-by: Belinda Liu <bliu@pivotal.io>

--- src/spec/gocli/integration/cli_env_spec.rb
+++ src/spec/gocli/integration/cli_env_spec.rb
@@ -8,6 +8,7 @@ describe 'cli: env', type: :integration do
   it 'outputs status' do
     out = bosh_runner.run('env')
     expect(out).to include("Using environment '#{current_sandbox.director_url}'")
-    expect(out).to include("Succeeded")
+    expect(out).to include('Stemcell version: -/-')
+    expect(out).to include('Succeeded')
   end
 end
